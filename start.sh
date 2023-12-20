50395ecc6294cd13dfc4c2f060305fb12c3b2a99
Robin <92713851+robinkutde@users.noreply.github.com>
2023-12-21 00:17:07 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,6 @@
 #!/bin/ash
 rm -rf /home/container/tmp/*
+mkdir /home/container/logs
 
 echo "⟳ Starting PHP-FPM..."
 /usr/sbin/php-fpm8 --fpm-config /home/container/php-fpm/php-fpm.conf --daemonize
